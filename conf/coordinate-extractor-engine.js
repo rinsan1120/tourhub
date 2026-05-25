81400e477e88babaf51f38f68154cf491fd1f0ca
rinsan1120 <keiz5963@gmail.com>
2026-05-25 10:03:33 +0900

    Update coordinate-extractor-engine.js

--- conf/coordinate-extractor-engine.js
+++ conf/coordinate-extractor-engine.js
@@ -4,7 +4,7 @@
  */
 (function() {
     // ★ここに後でGASのデプロイURLを入れる
-    const GAS_WEB_APP_URL = "YOUR_GAS_WEB_APP_URL_HERE";
+    const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbz5FIQdlOOridwOtGnMmWtqTfaE1KtDc5RodUX0rtfKvbwF7BMxqxRHcA-x5s1mmN1Z4Q/exec";
 
     window.addEventListener('DOMContentLoaded', () => {
         const inputUrl = document.getElementById('gmaps-url-input');
